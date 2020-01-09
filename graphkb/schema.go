46402f7cdf59758ee5fa7a3fb283452338d6ec32
Clement Michaud <clement.michaud34@gmail.com>
2020-01-09 14:40:18 +0100

    Create public interface by exporting internal types.

---
+++ graphkb/schema.go
@@ -0,0 +1,7 @@
+package graphkb
+
+import "github.com/clems4ever/go-graphkb/internal/schema"
+
+type AssetType = schema.AssetType
+type RelationKeyType = schema.RelationKeyType
+type RelationType = schema.RelationType
