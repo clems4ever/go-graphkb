483df086914bd759cbb455f87cc1777928026364
Clement Michaud <clement.michaud34@gmail.com>
2020-01-09 23:47:13 +0100

    Add travis tag to artifact name to publish artifact on GH.

--- .travis/run.sh
+++ .travis/run.sh
@@ -6,4 +6,3 @@ pushd web && npm ci && npm run build && popd
 go build -o go-graphkb cmd/go-graphkb/main.go
 go build -o importer-csv cmd/importer-csv/main.go
 
-tar cvf graphkb.tar.gz web/build go-graphkb importer-csv
