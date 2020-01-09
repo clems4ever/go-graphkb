7252608ade4169a2aa2d57e2eddfffb28fb9156f
Clement Michaud <clement.michaud34@gmail.com>
2020-01-09 10:35:39 +0100

    Add 'observed' relation between source and assets.

--- internal/knowledge/graph_updater.go
+++ internal/knowledge/graph_updater.go
@@ -25,24 +25,66 @@ func NewGraphUpdater(graphDB GraphDB, schemaPersistor schema.Persistor) *GraphUp
 	return &GraphUpdater{graphDB, schemaPersistor}
 }
 
-func (sl *GraphUpdater) doUpdate(updates SourceSubGraphUpdates) error {
-	previousSchema, err := sl.schemaPersistor.LoadSchema(context.Background(), updates.Source)
+// Augment the graph of the user with "observed" relation from the source to the each asset
+func (sl *GraphUpdater) appendObservedRelations(source string, updates *GraphUpdatesBulk) {
+	assetsToAdd := []Asset{Asset{Type: "source", Key: source}}
+	observedRelationsToRemove := []Relation{}
+	observedRelationsToAdd := []Relation{}
+
+	for _, a := range updates.AssetUpserts {
+		observedRelationsToAdd = append(observedRelationsToAdd, Relation{
+			Type: "observed",
+			From: AssetKey(assetsToAdd[0]),
+			To:   AssetKey(a),
+		})
+	}
+
+	for _, a := range updates.AssetRemovals {
+		observedRelationsToRemove = append(observedRelationsToRemove, Relation{
+			Type: "observed",
+			From: AssetKey(assetsToAdd[0]),
+			To:   AssetKey(a),
+		})
+	}
+
+	updates.AssetUpserts = append(updates.AssetUpserts, assetsToAdd...)
+	updates.RelationUpserts = append(updates.RelationUpserts, observedRelationsToAdd...)
+	updates.RelationRemovals = append(updates.RelationRemovals, observedRelationsToRemove...)
+}
+
+func (sl *GraphUpdater) updateSchema(source string, sg *schema.SchemaGraph) error {
+	for _, a := range sg.Assets() {
+		sg.AddRelation(schema.AssetType("source"), "observed", a)
+	}
+	sg.AddAsset("source")
+
+	previousSchema, err := sl.schemaPersistor.LoadSchema(context.Background(), source)
 	if err != nil {
 		fmt.Printf("[ERROR] Unable to read schema from DB: %v.\n", err)
 		fmt.Println("[WARNING] The graph has not been updated.")
 		return err
 	}
 
-	schemaEqual := previousSchema.Equal(updates.Schema)
+	schemaEqual := previousSchema.Equal(*sg)
 
 	if !schemaEqual {
 		fmt.Println("The schema needs an update")
-		if err := sl.schemaPersistor.SaveSchema(context.Background(), updates.Source, updates.Schema); err != nil {
+		if err := sl.schemaPersistor.SaveSchema(context.Background(), source, *sg); err != nil {
 			fmt.Printf("[ERROR] Unable to write schema in DB: %v.\n", err)
 			fmt.Println("[WARNING] The graph has not been updated.")
 			return err
 		}
 	}
+	return nil
+}
+
+func (sl *GraphUpdater) doUpdate(updates SourceSubGraphUpdates) error {
+	if err := sl.updateSchema(updates.Source, &updates.Schema); err != nil {
+		return err
+	}
+
+	sl.appendObservedRelations(updates.Source, &updates.Updates)
+
 	if err := sl.graphDB.UpdateGraph(updates.Source, &updates.Updates); err != nil {
 		fmt.Printf("[ERROR] Unable to write schema in graph DB: %v\n", err)
 		return err
