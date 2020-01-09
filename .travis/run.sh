f34a4f69761384e373ecf12e346ea8629f31c9c2
Clement Michaud <clement.michaud34@gmail.com>
2020-01-09 08:44:26 +0100

    Deploy graphkb on Github when branch is tagged.

---
+++ .travis/run.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+go test -v ./...
+
+pushd web && npm ci && npm run build && popd
+go build -o go-graphkb cmd/go-graphkb/main.go
+go build -o importer-csv cmd/importer-csv/main.go
+
+tar cvf graphkb.tar.gz web/build go-graphkb importer-csv
