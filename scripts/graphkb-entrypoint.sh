f5e13ad2258ba0edeb3d6e288e23c5a6919b162f
Clement Michaud <c.michaud@criteo.com>
2021-01-24 19:27:43 +0100

    Add a way to quickly reconfigure the graphkb.

--- scripts/graphkb-entrypoint.sh
+++ scripts/graphkb-entrypoint.sh
@@ -2,4 +2,4 @@
 
 go get github.com/cespare/reflex
 
-reflex -r '\.go|\.yml' -s -- sh -c "go run cmd/go-graphkb/main.go --config cmd/go-graphkb/config.yml listen"
+reflex -r '\.go|\.yml' -s -- sh -c "go run cmd/go-graphkb/main.go --config $1 listen"
