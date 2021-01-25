6e6b88a9038de5996c3c23e2922f87c70dc42952
Clement Michaud <c.michaud@criteo.com>
2021-01-25 13:29:36 +0100

    Add logs into the client + parallelize client upload.

--- internal/client/transaction.go
+++ internal/client/transaction.go
@@ -6,6 +6,8 @@ import (
 	"sync"
 	"time"
 
+	"gopkg.in/cheggaaa/pb.v2"
+
 	"github.com/clems4ever/go-graphkb/internal/knowledge"
 	"github.com/clems4ever/go-graphkb/internal/schema"
 )
@@ -64,33 +66,70 @@ func withRetryOnTooManyRequests(fn func() error, maxRetries int) error {
 func (cgt *Transaction) Commit() (*knowledge.Graph, error) {
 	sg := cgt.newGraph.ExtractSchema()
 
+	fmt.Println("Start uploading the schema of the graph...")
 	if err := cgt.client.UpdateSchema(sg); err != nil {
 		return nil, fmt.Errorf("Unable to update the schema of the graph")
 	}
 
+	fmt.Println("Finished uploading the schema of the graph...")
+
 	bulk := knowledge.GenerateGraphUpdatesBulk(cgt.currentGraph, cgt.newGraph)
 
-	for _, r := range bulk.GetRelationRemovals() {
-		if err := withRetryOnTooManyRequests(func() error { return cgt.client.DeleteRelation(r) }, 10); err != nil {
-			return nil, fmt.Errorf("Unable to remove the relation %v: %v", r, err)
+	fmt.Println("Start uploading the graph...")
+
+	progress := pb.New(len(bulk.GetAssetRemovals()) + len(bulk.GetAssetUpserts()) + len(bulk.GetRelationRemovals()) + len(bulk.GetRelationUpserts()))
+	progress.Start()
+	defer progress.Finish()
+
+	var wg sync.WaitGroup
+
+	wg.Add(2)
+
+	var err1, err2 error
+
+	go func() {
+		defer wg.Done()
+		for _, r := range bulk.GetRelationRemovals() {
+			if err := withRetryOnTooManyRequests(func() error { return cgt.client.DeleteRelation(r) }, 10); err != nil {
+				err1 = fmt.Errorf("Unable to remove the relation %v: %v", r, err)
+				return
+			}
+			progress.Increment()
 		}
+		for _, a := range bulk.GetAssetRemovals() {
+			if err := withRetryOnTooManyRequests(func() error { return cgt.client.DeleteAsset(a) }, 10); err != nil {
+				err1 = fmt.Errorf("Unable to remove the asset %v: %v", a, err)
+			}
+			progress.Increment()
+		}
+	}()
+
+	go func() {
+		defer wg.Done()
+		for _, a := range bulk.GetAssetUpserts() {
+			if err := withRetryOnTooManyRequests(func() error { return cgt.client.UpsertAsset(a) }, 10); err != nil {
+				err2 = fmt.Errorf("Unable to upsert the asset %v: %v", a, err)
+			}
+			progress.Increment()
+		}
+		for _, r := range bulk.GetRelationUpserts() {
+			if err := withRetryOnTooManyRequests(func() error { return cgt.client.UpsertRelation(r) }, 10); err != nil {
+				err2 = fmt.Errorf("Unable to upsert the relation %v: %v", r, err)
+			}
+			progress.Increment()
+		}
+	}()
+
+	wg.Wait()
+
+	if err1 != nil {
+		return nil, err1
 	}
-	for _, a := range bulk.GetAssetRemovals() {
-		if err := withRetryOnTooManyRequests(func() error { return cgt.client.DeleteAsset(a) }, 10); err != nil {
-			return nil, fmt.Errorf("Unable to remove the asset %v: %v", a, err)
-		}
+	if err2 != nil {
+		return nil, err2
 	}
 
-	for _, a := range bulk.GetAssetUpserts() {
-		if err := withRetryOnTooManyRequests(func() error { return cgt.client.UpsertAsset(a) }, 10); err != nil {
-			return nil, fmt.Errorf("Unable to upsert the asset %v: %v", a, err)
-		}
-	}
-	for _, r := range bulk.GetRelationUpserts() {
-		if err := withRetryOnTooManyRequests(func() error { return cgt.client.UpsertRelation(r) }, 10); err != nil {
-			return nil, fmt.Errorf("Unable to upsert the relation %v: %v", r, err)
-		}
-	}
+	fmt.Println("Finished uploading the graph...")
 
 	g := cgt.newGraph
 	cgt.newGraph = knowledge.NewGraph()
