61753897910392cb2e123fc6eeaebaf62ea27ce5
Mathilde Gilles <ma.gilles@criteo.com>
2022-10-27 16:00:41 +0200

    feat(client): add support for sources in query client

--- internal/client/types.go
+++ internal/client/types.go
@@ -46,23 +46,28 @@ type Column struct {
 	Type string `json:"type,omitempty"`
 }
 
-type Item map[string]string
+type Item map[string]any
 
 func (ra Item) Asset() knowledge.AssetWithID {
 	return knowledge.AssetWithID{
-		ID: ra["_id"],
+		ID: ra["_id"].(string),
 		Asset: knowledge.Asset{
-			Type: schema.AssetType(ra["type"]),
-			Key:  ra["key"],
+			Type: schema.AssetType(ra["type"].(string)),
+			Key:  ra["key"].(string),
 		},
 	}
 }
 
 func (ra Item) Relation() knowledge.RelationWithID {
 	return knowledge.RelationWithID{
-		ID:   ra["_id"],
-		From: ra["from_id"],
-		To:   ra["to_id"],
-		Type: schema.RelationKeyType(ra["type"]),
+		ID:   ra["_id"].(string),
+		From: ra["from_id"].(string),
+		To:   ra["to_id"].(string),
+		Type: schema.RelationKeyType(ra["type"].(string)),
 	}
+}
+
+func (ra Item) Sources() []string {
+	s, _ := ra["sources"].([]string)
+	return s
 }
